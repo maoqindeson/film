35aa2ecbda3eb2ce510bce6e1f9196582481c4a8
leo <hujingleo01@163.com>
2019-04-02 21:14:26 +0800

    代码健壮了

--- src/main/java/com/leoleo/film/controller/OrderController.java
+++ src/main/java/com/leoleo/film/controller/OrderController.java
@@ -42,7 +42,7 @@ public class OrderController {
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping("buy")
+    @RequestMapping("/buy")
     @RequiresAuthentication
     @Transactional(rollbackFor = Exception.class)
     public synchronized MaoqinObject buy(String orderid, String goodsid, int numbers, HttpServletRequest request) throws Exception {
@@ -52,29 +52,29 @@ public class OrderController {
         Price pr = priceService.getPriceByGoodsid(goodsid);
         BigDecimal price = pr.getPrice();
         if (user.getBalance().compareTo(price) < 0) {
-            return MaoqinObject.ok("余额不足");
+            return MaoqinObject.error("余额不足");
         }
         Goods go = goodsService.getGoodsByGoodsid(goodsid);
-        BigDecimal total =price.multiply(new BigDecimal(numbers));
-        BigDecimal balance = user.getBalance().subtract(total);
-        int updateResult =userService.updateBalance(name,balance);
-        if (0==updateResult ){                                                            //需要判断movieUserService.insertMovieUser结果是否成功，成功了才给用户更新余额userService.updateBalance
-            return MaoqinObject.ok("减余额失败，请重试");
+        synchronized (this) {
+            BigDecimal total = price.multiply(new BigDecimal(numbers));
+            BigDecimal balance = user.getBalance().subtract(total);
+            int updateResult = userService.updateBalance(name, balance);
+            if (0 == updateResult) {                                                            //需要判断movieUserService.insertMovieUser结果是否成功，成功了才给用户更新余额userService.updateBalance
+                return MaoqinObject.error("减余额失败，请重试");
+
+            }
+            if (updateResult > 1) {
+                throw new Exception();
+            }
+            if (go.getNumbers() < numbers) {
+                return MaoqinObject.error("库存不足");
+
+            }
+            int num = go.getNumbers() - numbers;
+            goodsService.updateGoods(goodsid, num);
+            orderService.insertOrder(orderid, name, goodsid, price, numbers, new Date(), "pay");
+            return MaoqinObject.ok(200, "购买成功", balance);
         }
-        if (updateResult>1){
-            throw new Exception();
-        }
-        if(go.getNumbers()<numbers){
-            return MaoqinObject.ok("库存不足");
-        }
-        int num = go.getNumbers()-numbers;
-        goodsService.updateGoods(goodsid,num);
-        orderService.insertOrder(orderid,name,goodsid,price,numbers,new Date(),"pay");
-          MaoqinObject maoqinObject = new MaoqinObject();
-          maoqinObject.setM(1);
-          maoqinObject.setMessage("购买成功");
-          maoqinObject.setObject(balance);
-          return maoqinObject;
     }
 
     @PostMapping("getOrderByOrderid")
@@ -87,11 +87,7 @@ public class OrderController {
     public MaoqinObject getOrderByPage(Integer pageNo, Integer pageSize) {
         int start = (pageNo - 1) * pageSize - pageSize;
         List<Order> list = orderService.getOrderByPage(start, pageSize);
-        MaoqinObject maoqinObject = new MaoqinObject();
-        maoqinObject.setM(1);
-        maoqinObject.setMessage("sucess");
-        maoqinObject.setObject(list);
-        return maoqinObject;
+        return MaoqinObject.ok(200, "sucess", list);
     }
 
     @PostMapping("getOrderGoodsNameByOrderid")
@@ -110,82 +106,55 @@ public class OrderController {
         return orderService.updateOrder(orderid, orderStatus);
     }
 
-    @PostMapping("payOrder")
+    @PostMapping("/payOrder")
     @RequiresAuthentication
-    @Transactional(rollbackFor = Exception.class)
-    public synchronized MaoqinObject payOrder(String orderid, HttpServletRequest request) {
-        MaoqinObject maoqinObject = new MaoqinObject();
+    @Transactional(rollbackFor = RuntimeException.class)
+    public MaoqinObject payOrder(String orderid, HttpServletRequest request) {
         String username = JWTUtil.getCurrentUsername(request);
         try {
             //校验入参
             if (StringTools.isNullOrEmpty(orderid)) {
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("需要有订单号");
-                return maoqinObject;
+                return MaoqinObject.error("需要有订单号");
             }
             Order order = orderService.getOrderByOrderid(orderid);
-            //TODO 这里缺少校验订单是否存在,万一是一个假的订单号,这里查到的order对象会是空的
-            if(order==null){
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("没有未支付的订单");
-                return maoqinObject;
+            if (order == null) {
+                return MaoqinObject.error("没有未支付的订单");
             }
             User user = userService.getUserByName(username);
-            Goods goods = goodsService.getGoodsByGoodsid(order.getGoodsid());
-            BigDecimal total = order.getPrice().multiply(new BigDecimal(order.getNumbers()));
-            if (order.getOrderStatus().equals("pay")) {
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("已支付的订单，不用重新支付");
-                return maoqinObject;
-            }
-            if (user.getBalance().compareTo(total) < 0) {
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("余额不足，请充值");
-                return maoqinObject;
-            }
-            if (goods.getNumbers() < order.getNumbers()) {
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("目前商品数量不足");
-                return maoqinObject;
-            }
+            synchronized (this) {
+                Goods goods = goodsService.getGoodsByGoodsid(order.getGoodsid());
+                BigDecimal total = order.getPrice().multiply(new BigDecimal(order.getNumbers()));
+                if (order.getOrderStatus().equals("pay")) {
+                    return MaoqinObject.error("已支付的订单，不用重新支付");
+                }
+                if (user.getBalance().compareTo(total) < 0) {
+                    return MaoqinObject.error("余额不足，请充值");
+                }
+                if (goods.getNumbers() < order.getNumbers()) {
+                    return MaoqinObject.error("目前商品数量不足");
+                }
 
-            BigDecimal balance = user.getBalance().subtract(total);
-            Integer blResult = userService.updateBalance(username, balance);
-            if (blResult == 0) {
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("支付失败，请重试");
-                return maoqinObject;
+                BigDecimal balance = user.getBalance().subtract(total);
+                Integer blResult = userService.updateBalance(username, balance);
+                if (blResult == 0) {
+                    return MaoqinObject.error("支付失败，请重试");
+
+                }
+                Integer nu = goods.getNumbers() - order.getNumbers();
+                Integer nuResult = goodsService.updateGoods(goods.getGoodsid(), nu);
+                if (nuResult == 0) {
+                    throw new RuntimeException();
+                }
+                Integer paresult = orderService.updateOrder(orderid, "pay");
+                if (paresult == 0) {
+                    throw new RuntimeException();
+                }
             }
-            Integer nu = goods.getNumbers() - order.getNumbers();
-            Integer nuResult = goodsService.updateGoods(goods.getGoodsid(), nu);
-            if (nuResult == 0) {
-                //TODO 这里当更新商品数量失败之后,将用户余额回退,思路是ok的,有更好的写法,比如使用事务回滚,
-                // TODO 可以参照上面 buy接口,使用@Transactional(rollbackFor = Exception.class)实现
-                throw new Exception();
-//                userService.updateBalance(username, user.getBalance());
-//                maoqinObject.setM(400);
-//                maoqinObject.setMessage("支付失败，请重试");
-//                return maoqinObject;
-            }
-            Integer paresult = orderService.updateOrder(orderid, "pay");
-            if (paresult == 0) {
-                throw new Exception();
-//                goodsService.updateGoods(goods.getGoodsid(), goods.getNumbers());
-//                userService.updateBalance(username, user.getBalance());
-//                maoqinObject.setM(400);
-//                maoqinObject.setMessage("支付失败，请重试");
-//                return maoqinObject;
-            }
-            maoqinObject.setM(1);
-            maoqinObject.setMessage("支付成功，谢谢惠顾");
-            maoqinObject.setObject(orderService.getOrderGoodsNameByOrderid(orderid));
-            return maoqinObject;
+            return MaoqinObject.ok(200, "支付成功，谢谢惠顾", orderService.getOrderGoodsNameByOrderid(orderid));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        maoqinObject.setM(400);
-        maoqinObject.setMessage("支付失败，请重试");
-        return maoqinObject;
+        return MaoqinObject.error("支付失败，请重试");
     }
 
     @PostMapping("newCreatOrder")
@@ -195,60 +164,36 @@ public class OrderController {
         String username = JWTUtil.getCurrentUsername(request);  //通过请求解密用户名
         try {
             if (StringTools.isNullOrEmpty(goodsid)) {        //校验货号入参
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("商品id不能为空");
-                return maoqinObject;
+                return MaoqinObject.ok(500, "商品id不能为空");
             }
             if (numbers == null || numbers == 0) {              //校验商品数量入参
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("商品数量不能为空或是0");
-                return maoqinObject;
+                return MaoqinObject.ok(500, "商品数量不能为空或是0");
             }
             Goods goods = goodsService.getGoodsByGoodsid(goodsid);    //新建商品查询的对象
             if (goods == null) {                      //校验商品是否存在
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("商品不存在");
-                return maoqinObject;
+                return MaoqinObject.ok(500, "商品不存在");
             }
             if (goods.getNumbers() < numbers) {              //校验商品数量是否够
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("商品数量不够");
+                return MaoqinObject.ok(500, "商品数量不够");
             }
             Price price = priceService.getPriceByGoodsid(goodsid);   //新建商品价格查询的对象
             if (price == null) {                        //校验商品价格是否存在
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("没有商品价格");
-                return maoqinObject;
+                return MaoqinObject.ok(500, "没有商品价格");
             }
             String orderid = StringTools.getTradeno();       //生成货号
-//        Order order = new Order();
-//        order.setOrderid(orderid);
-//        order.setGoodsid(goodsid);
-//        order.setName(username);
-//        order.setOrderStatus("no pay");
-//        order.setNumbers(numbers);
-//        order.setPrice(price.getPrice());
-//        order.setOrderTime(new Date());
             BigDecimal price1 = price.getPrice();
             Date orderTime = new Date();           //生成下单时间
             String orderStatus = "no pay";          //生成下单后的状态
             int result = orderService.insertOrder     //下单插入
                     (orderid, username, goodsid, price1, numbers, orderTime, orderStatus);
             if (result == 0) {                     //校验下单是否成功
-                maoqinObject.setM(400);
-                maoqinObject.setMessage("生成订单失败");
-                return maoqinObject;
+                return MaoqinObject.ok(500, "生成订单失败");
             }
-            maoqinObject.setM(1);
-            maoqinObject.setMessage("下单成功，尽快支付");
-            maoqinObject.setObject(orderService.getOrderGoodsNameByOrderid(orderid));
-            return maoqinObject;              //返回下单成功信息
+            return MaoqinObject.ok(500, "下单成功，尽快支付", orderService.getOrderGoodsNameByOrderid(orderid));              //返回下单成功信息
         } catch (Exception e) {
             e.printStackTrace();
         }
-        maoqinObject.setM(400);
-        maoqinObject.setMessage("下单异常");
-        return maoqinObject;        //下单有try catch来捕获异常，所以这里就是下单不成功，需要返回信息
+        return MaoqinObject.ok(500, "下单异常");        //下单有try catch来捕获异常，所以这里就是下单不成功，需要返回信息
     }
 
     @PostMapping("createOrder")
@@ -260,28 +205,20 @@ public class OrderController {
             String username = JWTUtil.getCurrentUsername(request);
             //先进行入参校验,如果是空则直接打回
             if (StringTools.isNullOrEmpty(goodsid)) {
-                maoqinObject.setM(500);
-                maoqinObject.setMessage("商品id不能为空");
-                return maoqinObject;
+                return MaoqinObject.error("商品id不能为空");
             }
             if (numbers == null || numbers == 0) {
-                maoqinObject.setM(500);
-                maoqinObject.setMessage("商品数量不能为空");
-                return maoqinObject;
+                return MaoqinObject.error("商品数量不能为空");
             }
             //校验商品id是否在数据库中能找到商品,如果不能则直接打回
             Goods goods = goodsService.selectById(goodsid);
             if (null == goods) {
-                maoqinObject.setM(500);
-                maoqinObject.setMessage("找不到对应的商品");
-                return maoqinObject;
+                return MaoqinObject.error("找不到对应的商品");
             }
             //查询商品的价格,如果找不到价格直接打回
             Price price = priceService.selectOne(new EntityWrapper<Price>().eq("goodsid", goodsid));
             if (null == price) {
-                maoqinObject.setM(500);
-                maoqinObject.setMessage("找不到商品对应的价格");
-                return maoqinObject;
+                return MaoqinObject.error("找不到商品对应的价格");
             }
             //初始化一个订单对象
             Order order = new Order();
@@ -300,20 +237,13 @@ public class OrderController {
             //将订单对象插入数据库,判断插入结果
             boolean result = orderService.insert(order);
             if (!result) {
-                maoqinObject.setM(500);
-                maoqinObject.setMessage("生成订单失败,请重试");
-                return maoqinObject;
+                return MaoqinObject.error("生成订单失败,请重试");
             }
-            maoqinObject.setM(200);
-            maoqinObject.setMessage("下单成功");
-            maoqinObject.setObject(orderService.getOrderGoodsNameByOrderid(orderId));
-            return maoqinObject;
+            return MaoqinObject.ok(200,"下单成功",orderService.getOrderGoodsNameByOrderid(orderId));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        maoqinObject.setM(500);
-        maoqinObject.setMessage("下单异常");
-        return maoqinObject;
+        return MaoqinObject.error("下单异常");
     }
 
 }
--- src/main/java/com/leoleo/film/utils/MaoqinObject.java
+++ src/main/java/com/leoleo/film/utils/MaoqinObject.java
@@ -2,49 +2,39 @@ package com.leoleo.film.utils;
 
 import lombok.Data;
 
+
 @Data
 public class MaoqinObject {
     private Integer m;
     private String message;
     private Object object;
 
-    public static MaoqinObject ok(String message) {
-        MaoqinObject maoqinObject =new MaoqinObject();
+    public static MaoqinObject error(String message ){
+        MaoqinObject maoqinObject = new MaoqinObject();
+        maoqinObject.setM(500);
+        maoqinObject.setMessage(message);
         return maoqinObject;
     }
 
-//    public Object getObject() {
-//        return object;
-//    }
-//
-//    public void setObject(Object object) {
-//        this.object = object;
-//    }
-//
-//    public int getM() {
-//        return m;
-//    }
-//
-//    public void setM(int m) {
-//        this.m = m;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-
-
-    public static MaoqinObject ok(Integer m){
-    MaoqinObject maoqinObject =new MaoqinObject();
-    return maoqinObject;
+    public static MaoqinObject ok(){
+        MaoqinObject maoqinObject =new MaoqinObject();
+        maoqinObject.setM(200);
+        maoqinObject.setMessage("success");
+        return maoqinObject;
     }
 
-    public static MaoqinObject ok(Object object){
+    public static MaoqinObject ok(Integer m ,String message) {
         MaoqinObject maoqinObject =new MaoqinObject();
+        maoqinObject.setM(m);
+        maoqinObject.setMessage(message);
+        return maoqinObject;
+    }
+
+    public static MaoqinObject ok(Integer m ,String message ,Object object){
+        MaoqinObject maoqinObject =new MaoqinObject();
+        maoqinObject.setM(m);
+        maoqinObject.setMessage(message);
+        maoqinObject.setObject(object);
         return maoqinObject;
     }
 }
