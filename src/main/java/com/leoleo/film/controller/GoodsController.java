e2b0cb48ff24b05a9babf4d47af2b2943be1383e
leo <hujingleo01@163.com>
2019-04-12 11:24:25 +0800

    删除了无用的接口

--- src/main/java/com/leoleo/film/controller/GoodsController.java
+++ src/main/java/com/leoleo/film/controller/GoodsController.java
@@ -15,36 +15,5 @@ import java.util.List;
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
-    @Autowired
-    private GoodsService goodsService;
 
-
-
-
-
-//    @PostMapping("/getGoodsByGoodsid")
-//    public Goods getGoodsByGoodsid(String goodsid) {
-//        Goods goods = goodsService.getGoodsByGoodsid(goodsid);
-//        return goods;
-//    }
-//
-//
-//    @PostMapping("/getGoodsByPage")
-//    public MaoqinObject getGoodsByPage(int pageNo,int pageSize){
-//        int start =(pageNo-1)*pageSize-pageSize;
-//        List<Goods>list = goodsService.getGoodsByPage(start,pageSize);
-//        MaoqinObject maoqinObject=new MaoqinObject();
-//        maoqinObject.setM(1);
-//        maoqinObject.setMessage("sucess");
-//        maoqinObject.setObject(list);
-//        return maoqinObject;
-//    }
-//    @PostMapping("/insertGoods")
-//    public int insertGoods(String goodsid,String goodsName,int numbers){
-//        return goodsService.insertGoods(goodsid,goodsName,numbers);
-//    }
-//    @PostMapping("/updateGoods")
-//    public int updateGoods(String goodsid,int numbers){
-//        return goodsService.updateGoods(goodsid,numbers);
-//    }
 }
